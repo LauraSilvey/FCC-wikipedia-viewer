832eecff40352f88b149c3238499acf40ae26fe9
Laura Silvey <lsilvey@gmail.com>
2016-07-01 05:48:37 -0700

    added search field and random wiki functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+function getRandomWiki () {
+  window.open("https://en.wikipedia.org/wiki/Special:Random"); 
+};
 
+function searchWiki () {
+  /*
+  store user entered query 
+  show 10 results on page
+  open new window when a result is clicked  
+  */
+};
+
+$(document).ready(function() {
+  $("#randomWiki").on("click", function(){
+  	getRandomWiki();
+  });
+});
