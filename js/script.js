cf4aa2ff1a8cb0c6626363be68be350a087007e0
Laura Silvey <lsilvey@gmail.com>
2016-07-05 07:03:44 -0700

    added media wiki api call

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
+var userQuery = "";
+
 function getRandomWiki () {
   window.open("https://en.wikipedia.org/wiki/Special:Random"); 
 };
 
 function searchWiki () {
-  /*
-  store user entered query 
+  // store user query in useable format
+  userQuery = $("#query").val().toLowerCase();
+  userQuery = encodeURI(userQuery);
+  
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + userQuery + "&callback=?";
+  
+  $.getJSON(url, function(data){
+    $("p").html(data[1][0]);
+  });
+  /* 
   show 10 results on page
   open new window when a result is clicked  
   */
@@ -14,4 +24,10 @@ $(document).ready(function() {
   $("#randomWiki").on("click", function(){
   	getRandomWiki();
   });
+
+  $("#searchButton").on("click", function() {
+    searchWiki();
+  });
+
+
 });
