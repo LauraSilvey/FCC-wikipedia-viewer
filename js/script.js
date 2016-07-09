c007ccc1c37bfff3132516aaa402d64602bfb1bb
Laura Silvey <lsilvey@gmail.com>
2016-07-09 07:01:13 -0700

    create newBox() and finish searchWiki()

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ function getRandomWiki () {
   window.open("https://en.wikipedia.org/wiki/Special:Random"); 
 };
 
+function newBox(title, description){
+  var newP = document.createElement("p");
+  var newHeader = document.createElement("h3")
+  var newDiv = document.createElement("div");
+      
+  $("body").append(newDiv);
+  $(newDiv).addClass("container well");
+  $(newDiv).append(newHeader, newP);
+  $(newHeader).addClass(title);
+  $(newP).addClass(description);
+};
+
 function searchWiki () {
   // store user query in useable format
   userQuery = $("#query").val().toLowerCase();
@@ -12,12 +24,16 @@ function searchWiki () {
   var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + userQuery + "&callback=?";
   
   $.getJSON(url, function(data){
-    $("p").html(data[1][0]);
+    for (var i = 0; i < 10; i ++){
+      var currentTitle = "title" + i;
+      var currentDescription = "description" + i;
+
+      newBox(currentTitle, currentDescription);
+      $('.' + currentTitle).html(data[1][0 + i]);
+      $('.' + currentTitle).wrap('<a href="' + data[3][0 + i] + '" target="blank" />');
+      $('.' + currentDescription).html(data[2][0 + i]);
+    }
   });
-  /* 
-  show 10 results on page
-  open new window when a result is clicked  
-  */
 };
 
 $(document).ready(function() {
@@ -28,6 +44,4 @@ $(document).ready(function() {
   $("#searchButton").on("click", function() {
     searchWiki();
   });
-
-
 });
