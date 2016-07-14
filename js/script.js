54823d430a317d3ad1f889a4e05f6ef12ecd5af2
Laura Silvey <lsilvey@gmail.com>
2016-07-14 07:01:18 -0700

    changed fonts and fixed edge case problem

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function newBox(title, description){
   var newHeader = document.createElement("h3")
   var newDiv = document.createElement("div");
       
-  $("body").append(newDiv);
+  $("#results").append(newDiv);
   $(newDiv).addClass("container well");
   $(newDiv).append(newHeader, newP);
   $(newHeader).addClass(title);
@@ -20,20 +20,22 @@ function searchWiki () {
   // store user query in useable format
   userQuery = $("#query").val().toLowerCase();
   userQuery = encodeURI(userQuery);
-  
-  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + userQuery + "&callback=?";
-  
-  $.getJSON(url, function(data){
-    for (var i = 0; i < 10; i ++){
-      var currentTitle = "title" + i;
-      var currentDescription = "description" + i;
 
-      newBox(currentTitle, currentDescription);
-      $('.' + currentTitle).html(data[1][0 + i]);
-      $('.' + currentTitle).wrap('<a href="' + data[3][0 + i] + '" target="blank" />');
-      $('.' + currentDescription).html(data[2][0 + i]);
-    }
-  });
+  if (userQuery.length > 0){
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + userQuery + "&callback=?";
+  
+    $.getJSON(url, function(data){
+      for (var i = 0; i < 10; i ++){
+        var currentTitle = "title" + i;
+        var currentDescription = "description" + i;
+
+        newBox(currentTitle, currentDescription);
+        $('.' + currentTitle).html(data[1][0 + i]);
+        $('.' + currentTitle).wrap('<a href="' + data[3][0 + i] + '" target="blank" />');
+        $('.' + currentDescription).html(data[2][0 + i]);
+      }
+    });
+  }
 };
 
 $(document).ready(function() {
